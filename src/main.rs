1b8ce64e4b08362317354e45cb7cf2d9d0ce733a
Dmitry Andreev <shaggydmitrydev@gmail.com>
2021-12-05 17:50:50 +0300

    Implemented guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io::stdin;
 
+const ERROR_MESSAGE: &str = "Не удалось прочитать информацию из stdio :(";
+
 fn main() {
-    println!("Угадай число!");
-    println!("Для того, чтобы продолжить, введите свое предположение :)");
+    let secret_number = rand::thread_rng().gen_range(1..21);
+    print_headline();
+    println!("Секретное число — {}", secret_number);
 
-    let mut guess = String::new();
-    let error_message = "Не удалось прочитать ввод из командной строки";
+    loop {
+        println!("А теперь попробуйте ввести свое предположение");
+        
+        let mut guess = String::new();
+        stdin()
+            .read_line(&mut guess)
+            .expect(ERROR_MESSAGE);
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    stdin()
-        .read_line(&mut guess)
-        .expect(&error_message);
+        println!("Ты загадал(-а): {}", guess);
 
-    println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Неверно, загаданное число больше"),
+            Ordering::Greater => println!("Неверно, загаданное число меньше"),
+            Ordering::Equal => {
+                println!("Поздравляю, вы выиграли!!!");
+                break;
+            }, 
+        }
+    }
+}
+
+fn print_headline() {
+    println!("ИГРА «УГАДАЙ ЧИСЛО»!");
+    println!("Я загадал тебе число от 1 до 20. Попробуй угадать)");
 }
